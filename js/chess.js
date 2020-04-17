0fabbec3592621dba7a37e15c73e07664dd92ff6
Ian Flom <ianrflom@gmail.com>
2020-04-17 14:16:46 -0500

    added pieces to board rendering

--- js/chess.js
+++ js/chess.js
@@ -1,4 +1,3 @@
-
 window.onload = () => {
 	const c = document.getElementById('chessboard');
 	drawBoard(c,'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
@@ -35,6 +34,9 @@ const drawBoard = (c, fen) => {
 			white ? ctx.fillStyle = '#ded3bd' : ctx.fillStyle  = '#a17b4d';
 
 			ctx.fillRect( i*(dim/8), j*(dim/8), dim/8, dim/8);
+			if (pieceArray[i][j] != 'e'){
+			ctx.drawImage(drawPiece(pieceArray[i][j],pieces),i*(dim/8),j*(dim/8),dim/8,dim/8)
+			}
 
 			white = !white;
 		}
@@ -43,3 +45,76 @@ const drawBoard = (c, fen) => {
 
 
 }
+
+const drawPiece = (val,set) => {
+	switch(String(val)){
+		case 'p':
+			return set.pd;
+		case 'P':
+			return set.pl;
+		case 'r':
+			return set.rd;
+		case 'R':
+			return set.rl;
+		case 'n':
+			return set.nd;
+		case 'N':
+			return set.nl;
+		case 'b':
+			return set.bd;
+		case 'B':
+			return set.bl;
+		case 'q':
+			return set.qd;
+		case 'Q':
+			return set.ql;
+		case 'k':
+			return set.kd;
+		case 'K':
+			return set.kl;
+		default:
+			return null;
+	}
+}
+
+const PieceSet = class {
+	constructor(source){
+		this.bd = new Image();
+		this.bd.src = `${source}/bd.svg`;
+
+		this.bl = new Image();
+		this.bl.src = `${source}/bl.svg`;
+
+		this.kd = new Image();
+		this.kd.src = `${source}/kd.svg`;
+
+		this.kl = new Image();
+		this.kl.src = `${source}/kl.svg`;
+
+		this.nd = new Image();
+		this.nd.src = `${source}/nd.svg`;
+
+		this.nl = new Image();
+		this.nl.src = `${source}/nl.svg`;
+
+		this.pd = new Image();
+		this.pd.src = `${source}/pd.svg`;
+
+		this.pl = new Image();
+		this.pl.src = `${source}/pl.svg`;
+
+		this.qd = new Image();
+		this.qd.src = `${source}/qd.svg`;
+
+		this.ql = new Image();
+		this.ql.src = `${source}/ql.svg`;
+
+		this.rd = new Image();
+		this.rd.src = `${source}/rd.svg`;
+
+		this.rl = new Image();
+		this.rl.src = `${source}/rl.svg`;
+
+	}
+}
+const pieces = new PieceSet('img/pieces');
